426f7ed8e7a47471a301e4c7093568407dc408ad
nk- <nenadkesic@gmail.com>
2024-06-17 15:51:47 +0200

    rc1.0

--- web/modules/custom/cchs_notifications/js/cchs_notifications.js
+++ web/modules/custom/cchs_notifications/js/cchs_notifications.js
@@ -1,94 +1,158 @@
 /**
  * @file
- * Manage subscription and unsubscription to Push notification
+ * Manage subscription and unsubscription for Push notifications.
  */
-((Drupal, once) => {
-  Drupal.behaviors.cchsNotification = {
-    attach: (context, settings) => {
+((Drupal, drupalSettings, once) => {
 
-      let title = Drupal.t('Successfully subscribed!');
-      const options = {
-        message: Drupal.t('Perhaps here should be this node info? Or ome custom text.')
-      };
-      doNotification(context, title, options);
+  /**
+   * Promise to check WebPush subscribe status.
+   */
+  const checkSubscribed = () => {
+    let alreadySubscribeStatus = false;
+    if (!WebPushSubscription.checkIsAvailable) {
+      alreadySubscribeStatus = true;
+    }
+    return navigator.serviceWorker
+      .getRegistration(WebPushSubscription.settings.webPush.serviceWorkerUrl)
+      .then(registration => {
 
-      const url = '/cch_notification/fetch';
-      fetchNotifications(context, url);
-      //.then((res) => {
-        // const message = JSON.parse(res[2].value);
-        // if (message && message.user && message.content) {
-        // options.message = message.content.body;
-        // title = message.parent_entity && message.parent_entity.title ? Drupal.t(message.parent_entity.title) : Drupal.t('@todo');
-        // const send = doNotification(context, title, options);
-        // send.then((r) => {
-        // r();
-        // });
-        // }
-      // });
+        if (!registration) {
+          alreadySubscribeStatus = true;
+        }
+        else {
+          registration.pushManager.getSubscription()
+            .then((subscription) => {
+              if (subscription) {
+                alreadySubscribeStatus = false;
+              }
+              else {
+                alreadySubscribeStatus = true;
+              }
+            });
+        }
+        return alreadySubscribeStatus;
+      });
+  };
+
+  /**
+   * Hook on core ajax DataCommand to register our subscription.
+   *
+   * @param {Drupal.Ajax} [ajax]
+   *   An Ajax object.
+   * @param {object} response
+   * @param {number} [status]
+   *   The HTTP status code.
+   */
+  Drupal.AjaxCommands.prototype.data = function (ajax, response, status,) {
+
+    if (!WebPushSubscription || !response) {
+      return;
+    }
+
+    if (response.command === 'data' && response.value && response.value.subscribe !== 'undefined') {
+
+      WebPushSubscription.settings = drupalSettings;
+      WebPushSubscription.registerSW();
+
+      /**
+       * Subscribe or un-subscribe - inherits from DANSE subscription.
+       */
+      checkSubscribed().then((subscribed) => {
+        if (response.value.subscribe) {
+          WebPushSubscription.subscribe();
+        }
+        else {
+          WebPushSubscription.unSubscribe();
+        }
+      });
     }
   };
 
-  async function fetchNotifications(context, url) {
-    const query = {};
-    const body = {};
-    const response = await fetch(
-      `${url}?${new URLSearchParams(query)}`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-type': 'application/json'
-        },
-        body: JSON.stringify(body)
-      }
-    );
-    if (response.ok) {
-      return await response.text(); //json();
-    }
-  }
+  // Drupal.behaviors.cchsNotification = {
+  //   attach: (context, settings) => {
+  //     // let title = Drupal.t('Successfully subscribed!');
+  //     // const options = {
+  //     //   message: Drupal.t('Perhaps here should be this node info? Or ome custom text.')
+  //     // };
+  //     // doNotification(context, title, options);
+  //     //
+  //     // const url = '/cch_notification/fetch';
+  //     // fetchNotifications(context, url);
+  //     //.then((res) => {
+  //       // const message = JSON.parse(res[2].value);
+  //       // if (message && message.user && message.content) {
+  //       // options.message = message.content.body;
+  //       // title = message.parent_entity && message.parent_entity.title ? Drupal.t(message.parent_entity.title) : Drupal.t('@todo');
+  //       // const send = doNotification(context, title, options);
+  //       // send.then((r) => {
+  //       // r();
+  //       // });
+  //       // }
+  //     // });
+  //   }
+  // };
 
-  async function doNotification(context, title, options) {
+  // async function fetchNotifications(context, url) {
+  //   const query = {};
+  //   const body = {};
+  //   const response = await fetch(
+  //     `${url}?${new URLSearchParams(query)}`,
+  //     {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-type': 'application/json'
+  //       },
+  //       body: JSON.stringify(body)
+  //     }
+  //   );
+  //   if (response.ok) {
+  //     return await response.text();
+  //   }
+  // }
 
-    const sendButton = document.querySelector('.danse-subscription-operation');
-    const registration = await navigator.serviceWorker.getRegistration();
+  // async function doNotification(context, title, options) {
+  //
+  //   const sendButton = document.querySelector('.danse-subscription-operation');
+  //   const registration = await navigator.serviceWorker.getRegistration();
+  //
+  //   const sendNotification = async () => {
+  //     if (Notification.permission === 'granted') {
+  //       showNotification(options.message);
+  //     }
+  //     else {
+  //       if (Notification.permission !== 'denied') {
+  //         const permission = await Notification.requestPermission();
+  //
+  //         if (permission === 'granted') {
+  //           showNotification(options.message);
+  //         }
+  //       }
+  //     }
+  //   };
+  //
+  //   const showNotification = body => {
+  //
+  //     const payload = {
+  //       body
+  //     };
+  //
+  //     let notification;
+  //     if (registration && 'showNotification' in registration) {
+  //       notification = registration.showNotification(title, payload);
+  //     }
+  //     else {
+  //       notification = new Notification(title, payload);
+  //     }
+  //     // notification.addEventListener("click", (e) => {
+  //     // clients.openWindow(
+  //     // "https://example.blog.com/2015/03/04/something-new.html",
+  //     // );
+  //     // });
+  //   };
+  //
+  //   sendButton.addEventListener('click', sendNotification);
+  //   return sendNotification;
+  //
+  // }
 
-    const sendNotification = async () => {
-      if (Notification.permission === 'granted') {
-        showNotification(options.message);
-      }
-      else {
-        if (Notification.permission !== 'denied') {
-          const permission = await Notification.requestPermission();
-
-          if (permission === 'granted') {
-            showNotification(options.message);
-          }
-        }
-      }
-    };
-
-    const showNotification = body => {
-
-      const payload = {
-        body
-      };
-
-      let notification;
-      if (registration && 'showNotification' in registration) {
-        notification = registration.showNotification(title, payload);
-      }
-      else {
-        notification = new Notification(title, payload);
-      }
-      // notification.addEventListener("click", (e) => {
-      // clients.openWindow(
-      // "https://example.blog.com/2015/03/04/something-new.html",
-      // );
-      // });
-    };
-
-    sendButton.addEventListener('click', sendNotification);
-    return sendNotification;
-
-  }
-
-})(Drupal, once);
+})(Drupal, drupalSettings, once);
--- web/modules/custom/cchs_notifications/src/Controller/WebPushSubscription.php
+++ web/modules/custom/cchs_notifications/src/Controller/WebPushSubscription.php
@@ -4,75 +4,132 @@ declare(strict_types=1);
 
 namespace Drupal\cchs_notifications\Controller;
 
-use Drupal\Component\Serialization\Json;
 use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\DataCommand;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\Session\AccountProxyInterface;
 use Drupal\Core\Url;
+use Drupal\danse\Service as DanseService;
 use Drupal\danse_content\Controller\Subscription;
-use GuzzleHttp\Client;
-use GuzzleHttp\Psr7\Request;
+use Drupal\danse_content\Service;
+use Drupal\user\UserDataInterface;
+use Drupal\web_push\Form\VAPIDForm;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Returns responses for cchs_notifications routes.
+ * Construct DANSE content controller extend.
+ *
+ * Adds up on DANSE subscriptions (on/off),
+ * in order to save/delete WebPush subscriptions based on it.
  */
 final class WebPushSubscription extends Subscription {
 
   /**
-   * List of routes to subscribe.
+   * Success and error message strings.
+   *
+   * @var array
    */
-  const DANSE_SUBSCRIBE_ROUTES = [
-    'danse_content.api.subscribe',
-    'danse_content.api.unsubscribe',
+  const MESSAGES = [
+    'success' => 'Successfully @op',
+    'error' => 'The Notification service is not correctly set. No <em>public key</em>',
   ];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(
+    EntityTypeManagerInterface $entity_type_manager,
+    DanseService $service,
+    Service $content_service,
+    UserDataInterface $user_data,
+    AccountProxyInterface $current_user,
+    private readonly ConfigFactoryInterface $configFactory,
+    private readonly MessengerInterface $messenger,
+  ) {
+    parent::__construct($entity_type_manager, $service, $content_service, $user_data, $current_user);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container): Subscription {
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('danse.service'),
+      $container->get('danse_content.service'),
+      $container->get('user.data'),
+      $container->get('current_user'),
+      $container->get('config.factory'),
+      $container->get('messenger')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function subscribe(string $entity_type, string $entity_id, string $key): AjaxResponse {
     $this->userData->set('danse', $this->currentUser->id(), $key, 1);
-    /* @todo Make this request works for WebPush subscription entity save */
-    /* $this->webPushSubscribe(); */
-    return $this->widget($entity_type, $entity_id);
+    $response = parent::subscribe($entity_type, $entity_id, $key);
+    $this->modifyResponse($response);
+    return $response;
   }
 
   /**
    * {@inheritdoc}
    */
   public function unsubscribe(string $entity_type, string $entity_id, string $key): AjaxResponse {
-    // For now, just do parent call.
-    return parent::unsubscribe($entity_type, $entity_id, $key);
+    $response = parent::unsubscribe($entity_type, $entity_id, $key);
+    $this->modifyResponse($response, FALSE);
+    return $response;
   }
 
   /**
-   * Create or delete WebPush subscription entity.
+   * Add some data and make ready for WebPush subscription entity.
+   *
+   * @param \Drupal\Core\Ajax\AjaxResponse $response
+   *   Ajax response object returned from parent, DANSE controller.
+   * @param bool $op
+   *   Subscribe or un-subscribe flag.
    */
-  private function webPushSubscribe(): void {
-    $token_url = Url::fromRoute('system.csrftoken', [], ['absolute' => TRUE])->toString();
-    $subscribe_url = Url::fromRoute('rest.web_push_subscription.POST', [], ['absolute' => TRUE])->toString();
-    $client = new Client();
-    $headers = [
-      // 'Authorization' => 'Bearer ' . $token,
-      'Accept' => 'application/json',
-      'Content-Type' => 'application/json',
-    ];
-    $body_data = [
-      'key' => NULL,
-      'token' => NULL,
-      'endpoint' => NULL,
-    ];
-    $body = Json::encode($body_data);
-    $request = new Request('POST', $subscribe_url, [
-      'json' => $body_data,
-      'headers' => $headers,
-    ]);
-    $promise = $client->sendAsync($request)->then(function ($r) {
-      // $subscription = $this->entityTypeManager->getStorage('web_push_subscription')->create([
-      // 'key' => $key,
-      // 'token' => $token,
-      // 'endpoint' => $endpoint,
-      // ]);
-      // $subscription->save();
-    });
-    $promise->wait();
+  private function modifyResponse(AjaxResponse &$response, bool $op = TRUE): void {
+
+    // Get the VAPID Public Key to add it in JS notification subscription.
+    $config = $this->configFactory->get(VAPIDForm::$configId);
+    $publicKey = $config->get('publicKey');
+
+    // Check if the public key is set.
+    if (!empty($publicKey)) {
+      $response->addAttachments([
+        'library' => [
+          'cchs_notifications/notify',
+        ],
+      ]);
+
+      $settings = [
+        'publicKey' => $publicKey,
+        'serviceWorkerUrl' => Url::fromRoute('web_push.service_worker')->toString(),
+        'subscribeUrl' => Url::fromRoute('rest.web_push_subscription.POST')->toString(),
+      ];
+      $attachments['drupalSettings']['webPush'] = $settings;
+      $response->addAttachments($attachments);
+
+      // A Flag, subscribe or un-subscribe.
+      $data = [
+        'subscribe' => $op,
+      ];
+      $response->addCommand(new DataCommand('', 'cchs_notifications', $data));
+      $subscribe_label = $op ? 'subscribed' : 'unsubscribed';
+      $this->messenger->addMessage($this->t('@message', [
+        '@message' => self::MESSAGES['success'] . ' ' . $subscribe_label,
+      ]));
+    }
+    else {
+      $this->messenger->addError($this->t('@message', [
+        '@message' => self::MESSAGES['error'],
+      ]));
+    }
   }
 
 }
